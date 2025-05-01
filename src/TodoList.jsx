2e8968941cc5e2c08a63811af37af3e09743e5c9
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-05-01 19:29:13 +0530

    Update all and Update one

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import { v4 as uuidv4 } from 'uuid';
 
 export default function TodoList() {
-    let [todos, setTodos] = useState([{ task: "Eat", id: uuidv4() }])
+    let [todos, setTodos] = useState([{ task: "Eat", id: uuidv4(), isDone: false }])
     let [newTodo, setNewTodo] = useState("")
 
     let addNewTask = () => {
@@ -18,8 +18,33 @@ export default function TodoList() {
 
     let deleteTodo = (id) => {
         setTodos((prevTodos) => prevTodos.filter((prevTodos) => prevTodos.id != id))  //filter method returns us new copy
+    }
 
+    let upperCaseAll = () => {
+        setTodos((prevTodos) =>
+            prevTodos.map((todo) => {
+                return {
+                    ...todo,
+                    task: todo.task.toUpperCase()
+                }
+            })
+        )
+    }
 
+    let upperCaseOne = (id) => {
+        setTodos((prevTodos) =>
+            prevTodos.map((todo) => {
+                if (todo.id == id) {
+                    return {
+                        ...todo,
+                        task: todo.task.toUpperCase()
+                    }
+                } else {
+                    return todo
+                }
+
+            })
+        )
     }
     return (
         <div>
@@ -37,10 +62,13 @@ export default function TodoList() {
                             <span> {todo.task}</span>
                             &nbsp;&nbsp;&nbsp;
                             <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+                            <button onClick={() => upperCaseOne(todo.id)}>UpperCase One</button>
                         </li>
                     ))
                 }
             </ul>
+            <br></br>
+            <button onClick={upperCaseAll}>UpperCase All</button>
         </div>
     )
 }
